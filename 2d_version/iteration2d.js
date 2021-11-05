d56da880ff69dc6fec6a512f055461202fe05c95
Wokkta <55146833+Wokkta@users.noreply.github.com>
2021-11-05 16:54:33 +0300

    Update iteration2d.js

--- 2d_version/iteration2d.js
+++ 2d_version/iteration2d.js
@@ -106,16 +106,3 @@ consoleMatrix(
 );
 
 console.log("end");
-
-function testFinding(matrix) {
-  let indexesMatrix = [];
-  for (let i = 0; i < matrix.length; i++) {
-    let row = [];
-    for (let j = 0; j < matrix[i].length; j++) {
-      let summ = summar(findingNeibors(i, j, matrix), matrix);
-      row.push(summ);
-    }
-    indexesMatrix.push(row);
-  }
-  return indexesMatrix;
-}
